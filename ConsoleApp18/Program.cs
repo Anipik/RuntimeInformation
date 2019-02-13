7b811c72e18b8b0531592fe398473c651e919bd9
Anipik <anirudhagnihotry098@gmail.com>
2019-02-12 16:24:38 -0800

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RuntimeInformation.OSDescription.Contains("Ubuntu", StringComparison.InvariantCultureIgnoreCase));
+            Console.ReadLine();
+        }
+    }
+}
